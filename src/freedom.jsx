decb1e3c4bc0c268d01524faf21e81668cbd41c8
Tonypaul544 <paulnwodotochukwu180@gmail.com>
2026-03-17 14:32:20 +0100

    corol change

--- src/freedom.jsx
+++ src/freedom.jsx
@@ -1,7 +1,7 @@
 export default function Freedom() {
   return (
     <div className="flex flex-col bg-gray-600/10 rounded-2xl my-10 mx-auto px-4 sm:px-8 md:px-20 py-10 items-center justify-center max-w-7xl w-full">
-      <h1 className="text-xl font-bold text-center mt-10 text-[#CC0000]">Your journey to financial freedom starts here.</h1>
+      <h1 className="text-xl font-bold text-center mt-10 text-[#CC0000]!">Your journey to financial freedom starts here.</h1>
       <p className="text-center mt-4 text-lg text-gray-300 max-w-4xl">
         Join the JV Mastery Program and unlock the secrets to building wealth, achieving financial independence, and living life on your own terms. With our proven strategies and expert guidance, you'll gain the knowledge and confidence to take control of your financial future.
       </p>
