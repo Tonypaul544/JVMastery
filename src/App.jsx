491684e611ee951426222c423c0bf90b4d8938f5
Tonypaul544 <paulnwodotochukwu180@gmail.com>
2026-03-17 13:52:13 +0100

    change button color

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function HeroSection() {
             transition={{ duration: 0.5, delay: 0.6 }}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
-            className="mt-6 inline-flex items-center justify-center rounded-xl bg-red-600 px-10 py-2 text-lg font-semibold shadow-lg shadow-red-600/40 transition hover:bg-red-700"
+            className="mt-6 inline-flex items-center justify-center rounded-xl bg-[#BE0101] px-10 py-2 text-lg font-semibold shadow-lg shadow-red-600/40 transition hover:bg-red-700"
           >
             Sign Up Today
           </motion.button>
